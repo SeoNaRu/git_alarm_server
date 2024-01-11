58814cfc02c324ad8c2ee62688481be244415c27
SeoNaru <tjehddnr12345@naver.com>
2024-01-11 21:48:31 +0900

    add: main

--- app.module.ts
+++ app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+import { ArticleModule } from './article/article.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ArticleModule,
+    UserModule,
+    MongooseModule.forRoot(process.env.DB_URI, {
+      dbName: 'with_tft',
+    }),
+  ],
 })
 export class AppModule {}
--- main.ts
+++ main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.listen(8080);
+    console.log(`Application is running on: http://localhost:8080`);
+  } catch (error) {
+    console.error('Error during application bootstrap:', error);
+  }
 }
+
 bootstrap();
