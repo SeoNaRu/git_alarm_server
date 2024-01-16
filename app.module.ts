6facaa955477a2f5cae4d746f8de8cce4a318126
SeoNaru <tjehddnr12345@naver.com>
2024-01-16 12:10:53 +0900

    일부 버그 수정

--- app.module.ts
+++ app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
-import { ArticleModule } from './article/article.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    ArticleModule,
+
     UserModule,
     MongooseModule.forRoot(process.env.DB_URI, {
       dbName: 'with_tft',
